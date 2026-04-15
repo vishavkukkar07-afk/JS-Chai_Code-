23997399da8716c5a46b969d0922efc2f338e62c
vishavkukkar07-afk <vishavkukkar07@gmail.com>
2026-04-15 05:07:53 +0000

    Conversion of datatypes

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,16 @@
+let score =  45;
+
+console.log(score);
+console.log(typeof score);
+
+let isloogedin = 0;
+
+let booleanloggedin = Boolean(isloogedin);
+
+console.log(booleanloggedin);
+
+// 1- true 
+// 0- false
+
+// ""- false
+// "vishav"- true
